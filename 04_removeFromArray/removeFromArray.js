bdba471d15fb8859bf62805f61b2829aa3e42d7b
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 10:54:38 +0530

    Solve 4th JS excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToBeRemoved) {
+    newArray = []
+    for (const val of array) {
+        if (!argsToBeRemoved.includes(val)) {
+            newArray.push(val);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
