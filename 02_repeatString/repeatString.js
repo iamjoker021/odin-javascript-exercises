f1b727a26f6e9e971c6949649c240353792edb33
iamjoker021 <iamjoker021@gmail.com>
2024-07-04 10:29:40 +0530

    Solve 1st and 2nd task on JS excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    if (numOfTimes < 0) {
+        return 'ERROR'
+    }
+    return string.repeat(numOfTimes);
 };
 
 // Do not edit below this line
