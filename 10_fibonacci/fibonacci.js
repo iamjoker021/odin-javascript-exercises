e715f64b808e912130e60fc072830773a894e3f8
iamjoker021 <iamjoker021@gmail.com>
2024-07-05 09:51:58 +0530

    Solve 10th JS task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(nthFibococi) {
+    if (nthFibococi < 0) {
+        return "OOPS"
+    }
+    else if (nthFibococi == 0) {
+        return 0;
+    }
 
+    let num1 = 1, num2 = 1, num3;
+    for (let i = 2; i < nthFibococi; i++) {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+    return num3 || num2;
 };
 
 // Do not edit below this line
