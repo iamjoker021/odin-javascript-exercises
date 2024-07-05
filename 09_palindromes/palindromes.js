8be2c19ab10a993944ba8edceb5811a0ded0471f
iamjoker021 <iamjoker021@gmail.com>
2024-07-05 09:38:56 +0530

    Solve 9th JS execrecise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, '')
+    for (let i = 0; i < word.length/2; i++) {
+        if (word[i] != word[word.length-i-1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
