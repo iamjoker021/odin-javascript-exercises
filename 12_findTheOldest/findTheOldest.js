bdf6e36c23f176b214884da46cb2b825ed3a1226
iamjoker021 <iamjoker021@gmail.com>
2024-07-05 10:08:30 +0530

    Solve 12th JS excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        let age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+        if (!oldestPerson.age || oldestPerson.age < age) {
+            oldestPerson["age"] = age;
+            oldestPerson["name"] = person.name;
+        }
+        return oldestPerson;
+    }, {});
 };
 
 // Do not edit below this line
